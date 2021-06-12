6552bbcf0945346649fb9416c6169cf621c63d58
jujubebat <bbwwpark@naver.com>
2021-06-12 16:15:51 +0900

    feat: 엔티티들의 등록일, 수정일을 자동 저장하는 기능 구현

--- src/test/java/com/jujubebat/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jujubebat/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.jujubebat.book.springboot.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -43,6 +44,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }

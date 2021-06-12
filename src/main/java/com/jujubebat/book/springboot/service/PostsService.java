5ef70e6c09fffa093b073268329f0ff2e26f2649
jujubebat <bbwwpark@naver.com>
2021-06-12 17:06:30 +0900

    feat: 게시글 전체 조회 기능 및 화면 구현

--- src/main/java/com/jujubebat/book/springboot/service/PostsService.java
+++ src/main/java/com/jujubebat/book/springboot/service/PostsService.java
@@ -5,9 +5,11 @@ import com.jujubebat.book.springboot.domain.posts.PostsRepository;
 import com.jujubebat.book.springboot.web.dto.PostsResponseDto;
 import com.jujubebat.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jujubebat.book.springboot.web.dto.PostsUpdateRequestDto;
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -27,10 +29,19 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
             .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/jujubebat/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/jujubebat/book/springboot/web/dto/PostsListResponseDto.java
@@ -5,20 +5,17 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
-public class PostsResponseDto {
+public class PostsListResponseDto {
 
     private Long id;
     private String title;
-    private String content;
     private String author;
     private LocalDateTime modifiedDate;
 
-    public PostsResponseDto(Posts entity) {
+    public PostsListResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.content = entity.getContent();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
     }
-
 }

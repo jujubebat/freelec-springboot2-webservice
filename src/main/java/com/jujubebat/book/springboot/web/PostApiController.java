26c673a3c281b99a5a7327d905a66265ef97e40e
jujubebat <bbwwpark@naver.com>
2021-06-12 16:00:23 +0900

    feat: 게시글 조회, 수정 api 구현

--- src/main/java/com/jujubebat/book/springboot/web/PostApiController.java
+++ src/main/java/com/jujubebat/book/springboot/web/PostApiController.java
@@ -1,9 +1,15 @@
 package com.jujubebat.book.springboot.web;
 
 import com.jujubebat.book.springboot.service.PostsService;
+import com.jujubebat.book.springboot.web.dto.PostsResponseDto;
 import com.jujubebat.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jujubebat.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +22,16 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 
 }

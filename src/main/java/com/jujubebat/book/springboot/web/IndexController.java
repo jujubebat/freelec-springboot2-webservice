417d1336d9f40f272fff023a5778bf1c6f0c52f4
jujubebat <bbwwpark@naver.com>
2021-06-13 19:04:53 +0900

    feat: 로그인 기능에 아규먼트 리졸버 적용

--- src/main/java/com/jujubebat/book/springboot/web/IndexController.java
+++ src/main/java/com/jujubebat/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jujubebat.book.springboot.web;
 
+import com.jujubebat.book.springboot.config.auth.LoginUser;
 import com.jujubebat.book.springboot.config.auth.SessionUser;
 import com.jujubebat.book.springboot.service.PostsService;
 import com.jujubebat.book.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());

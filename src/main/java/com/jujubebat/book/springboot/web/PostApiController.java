87eaaa4f07305c301175e890de65d2e84d3522db
jujubebat <bbwwpark@naver.com>
2021-06-13 16:45:48 +0900

    feat: 게시글 삭제 기능 및 화면 구현

--- src/main/java/com/jujubebat/book/springboot/web/PostApiController.java
+++ src/main/java/com/jujubebat/book/springboot/web/PostApiController.java
@@ -4,8 +4,8 @@ import com.jujubebat.book.springboot.service.PostsService;
 import com.jujubebat.book.springboot.web.dto.PostsResponseDto;
 import com.jujubebat.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jujubebat.book.springboot.web.dto.PostsUpdateRequestDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,13 +25,18 @@ public class PostApiController {
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
+    public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
